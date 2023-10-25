780663f061bbff2abd9d2de86c06516f01f4adef
suspect <79446154+wevez@users.noreply.github.com>
2023-10-25 13:03:23 +0900

    Update CustomYT4J.java

--- src/CustomYT4J.java
+++ src/CustomYT4J.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CustomYT4J extends YT4J {
@@ -14,51 +13,44 @@ public class CustomYT4J extends YT4J {
 
     @Override
     protected String getHTTP(String URL, String USER_AGENT) {
-        URL requestUrl;
         try {
-            requestUrl = new URL(URL);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+            final URL requestUrl = new URL(URL);
 
-        HttpURLConnection connection;
-        try {
-            connection = (HttpURLConnection) requestUrl.openConnection();
+            final HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
             connection.setRequestProperty("User-Agent", USER_AGENT);
             connection.setRequestMethod("GET");
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
-            StringBuilder response = new StringBuilder();
+            final StringBuilder response = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 response.append(line);
             }
             reader.close();
             connection.disconnect();
             return response.toString();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     protected String postHTTP(String URL, String USER_AGENT, JsonObject JSON) {
-        URL url;
         try {
-            url = new URL(URL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            final URL url = new URL(URL);
+            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setDoOutput(true);
 
-            OutputStream outputStream = connection.getOutputStream();
+            final OutputStream outputStream = connection.getOutputStream();
             outputStream.flush();
             outputStream.write(JSON.toString().getBytes());
             outputStream.close();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
-            StringBuilder response = new StringBuilder();
+            final StringBuilder response = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 response.append(line);
             }
@@ -66,7 +58,7 @@ public class CustomYT4J extends YT4J {
             connection.disconnect();
 
             return response.toString();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new RuntimeException(e);
         }
     }
